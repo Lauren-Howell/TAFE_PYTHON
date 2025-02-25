f7421005eb21e56bbf2ad119f7fc35cbb51849c3
Skye0204 <79391035+Skye0204@users.noreply.github.com>
2025-02-25 04:37:23 +0000

    nearly finished

--- Login Script.py
+++ Login Script.py
@@ -1,15 +1,46 @@
-import tkinter
-from tkinter import *
-f = open("accounts.txt")
-root = Tk()
-Label(root, text='Username:').grid(row=0, padx=(10, 10))
-Label(root, text='Password:').grid(row=1, padx=(10, 10))
-e1 = Entry(root)
-e2 = Entry(root, show="*")
-e1.grid(row=0, column=1, padx=10, pady=10)
-e2.grid(row=1, column=1, padx=10, pady=10)
-button = tkinter.Button(root, text="New User", width=25)
-button.grid(row=2, padx=10, pady=10, columnspan=3)
-button = tkinter.Button(root, text="Reset Password", width=25)
-button.grid(row=3, padx=10, pady=10, columnspan=3)
-mainloop()
+# pwFile = open("accounts.txt", "r")
+userExists = 0
+
+while userExists <= 0:
+    answer = input("welcome to Gelos portal would you like to: login(l), create an account(c)?")
+
+    if answer == "l":
+        username = input("enter your username ")
+        password = input("enter your password ")
+    
+        with open('accounts.txt', 'r') as pwFile:
+            for line in pwFile:
+                u,p = line.strip().split(",")
+            if username == u and password == p:
+                userExists = 1
+                loggedIn = True
+                print("success")
+                break
+            else:
+                    print("fail")
+
+    elif answer == "c":
+        username = input("enter your prefered username ")
+        password = input("enter your preferred password ")
+
+        with open('accounts.txt', 'a') as pwFile:
+            pwFile.write(username + "," + password + "\n")
+            print("success")
+            userExists = -1
+            pwFile.close
+            
+        
+    else:
+        print("try again")
+
+while loggedIn == True and userExists == 1:
+    response = input("Would you like a list of users? yes/no")
+    if response == "yes":
+        with open('accounts.txt', 'r') as pwFile:
+            for lines in pwFile:
+                u,p = lines.strip().split(",")
+                print(u)
+                break
+    else:
+        print("Have a nice day!")
+        break
